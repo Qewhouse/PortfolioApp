a223aebba67d649b910888bb09011cb0613b726f
Alexander Altman <qewhouse@gmail.com>
2023-08-01 21:12:07 +0300

    Initial Commit

---
+++ PortfolioApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PortfolioApp
+//
+//  Created by Alexander Altman on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
